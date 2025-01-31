66a56ba4a958202fe4157fd74c6ab8b0b6145215
bioimagehub <bioimagehub@gmail.com>
2025-01-31 12:49:12 +0100

    Update metadata.go

--- bfmetadata/metadata.go
+++ bfmetadata/metadata.go
@@ -1,31 +1,3 @@
-package bfmetadata
-
-import (
-	"bytes"
-	_ "embed"
-	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
-)
-
-// Embed bfconvert.bat
-//
-//go:embed bftools/bfconvert.bat
-var bfconvertBat []byte
-
-// Embed bioformats_package.jar
-//
-//go:embed bftools/bioformats_package.jar
-var bioformatsJar []byte
-
-// Embedding bf.bat
-//
-//go:embed bftools/bf.bat
-var bfBat []byte
-
-// PrintHelp executes the embedded bfconvert.bat with the --help flag and returns the output.
 func PrintHelp() (string, error) {
 	// Create a temporary directory
 	tempDir, err := os.MkdirTemp("", "bfconvert")
@@ -41,25 +13,20 @@ func PrintHelp() (string, error) {
 		return "", fmt.Errorf("error writing bf.bat to temp file: %w", err)
 	}
 
-	// Write bfconvert.bat to the temporary file
+	// Write other files like bfconvert.bat and bioformats_package.jar
 	batFile := filepath.Join(tempDir, "bfconvert.bat")
 	err = os.WriteFile(batFile, bfconvertBat, 0755)
 	if err != nil {
 		return "", fmt.Errorf("error writing bfconvert.bat to temp file: %w", err)
 	}
 
-	// Write bioformats_package.jar to the temporary file
 	jarFile := filepath.Join(tempDir, "bioformats_package.jar")
 	err = os.WriteFile(jarFile, bioformatsJar, 0644)
 	if err != nil {
 		return "", fmt.Errorf("error writing bioformats_package.jar to temp file: %w", err)
 	}
 
-	// Debugging: Print the paths
-	fmt.Printf("Using temporary BFCONVERTPATH: %s\n", batFile)
-	fmt.Printf("Using temporary BIOFORMATSPATH: %s\n", jarFile)
-
-	// Adjust environment variables to point to the temp directory
+	// Set environment and execute the command using the temporary files
 	cmd := exec.Command("cmd", "/C", batFile, "--help")
 	cmd.Env = append(os.Environ(), fmt.Sprintf("BF_DIR=%s", tempDir))
 
@@ -74,6 +41,5 @@ func PrintHelp() (string, error) {
 		return out.String(), fmt.Errorf("error executing bfconvert.bat --help: %w, raw stderr: %s", err, stderr.String())
 	}
 
-	// Return the help message
 	return out.String(), nil
 }
