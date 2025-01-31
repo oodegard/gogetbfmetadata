16bb9b57a9bdb872aecb8cb6733eb0d081c09363
bioimagehub <bioimagehub@gmail.com>
2025-01-31 11:38:02 +0100

    remove env

--- main.go
+++ main.go
@@ -5,16 +5,9 @@ import (
 	"log"
 
 	"gogetbfmetadata/bfmetadata"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Load the .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
 
 	// Call the function to print the help info
 	helpMessage, err := bfmetadata.PrintHelp()
